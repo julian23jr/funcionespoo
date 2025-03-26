1d870537e08ca16c05541241f2a4d8772610b578
julian23jr <1023371754@teinco.edu.co>
2025-03-26 10:51:21 -0500

    Update funcionuno.py

---
+++ funcionuno.py
@@ -0,0 +1,8 @@
+# declaracion de la fincion
+def suma(a,b):
+    return a + b 
+   
+    #seleccionar de los argumentos 
+    resultado = suma (25,32)
+    # impresion del resultados 
+    print(resultado)
